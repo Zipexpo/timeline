37ae090eeb0aebe3fb178cb61490e112d049140d
Zipexpo <nvtngan@hcmus.edu.vn>
2019-03-21 09:30:54 -0500

    update

--- src/script/main.js
+++ src/script/main.js
@@ -11,7 +11,7 @@ $(document).ready(function () {
     legendmaker();
 });
 var startDate = "Aug 27 2018";
-var endDate = "Feb 31 2019";
+var endDate = new Date();
 function main(){
     d3.queue()
     // .defer(d3.json,"src/data/twittwaterv2.json")
